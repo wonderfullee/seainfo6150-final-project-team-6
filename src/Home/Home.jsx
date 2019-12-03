daf48194b57f8f7ea196f3462544f6f186692bc7
jiawei2018 <35362950+jiawei2018@users.noreply.github.com>
2019-12-03 13:46:05 -0800

    change svg logo into H1 text logo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -13,33 +13,34 @@ const Home = () => {
         <div className= "main">
 
             <div className = "logo">
-                <img className= "logimg" src={logo} alt = "logo"/>
+                <h1 className={"h1"}>NEU-restaurant </h1>
+                {/*<img className= "logimg" src={logo} alt = "logo"/>*/}
             </div>
 
                 <div className="cateInner">
 
                     <div className="img">
-                    <text className="foodText">Chinese Food</text>
+                    <span className="foodText">Chinese Food</span>
                     <img className={"pic"} src= {cnf} alt="chinese" onClick={() => {window.location.href="/restaurants/chinese"}}/>
                     </div>
 
                     <div className="img">
-                        <text className="foodText">Italian Food</text>
+                        <span className="foodText">Italian Food</span>
                     <img className={"pic"} src={itl} alt="italian" onClick={() => {window.location.href="/restaurants/italian"}}/>
                     </div>
 
                     <div className="img">
-                        <text className="foodText">Japanese Food</text>
+                        <span className="foodText">Japanese Food</span>
                     <img className={"pic"} src={jap} alt="japanese" onClick={() => {window.location.href="/restaurants/japanese"}}/>
                     </div>
 
                     <div className="img">
-                        <text className="foodText">Mexican Food</text>
+                        <span className="foodText">Mexican Food</span>
                     <img className={"pic"}  src={mex} alt="mexican" onClick={() => {window.location.href="/restaurants/mexican"}}/>
                     </div>
 
                     <div className="img">
-                        <text className="foodText">Trad-American Food</text>
+                        <span className="foodText">Trad-American Food</span>
                     <img className={"pic"} src={tra} alt="tradamerican" onClick={() => {window.location.href="/restaurants/tradamerican"}}/>
                     </div>
 
