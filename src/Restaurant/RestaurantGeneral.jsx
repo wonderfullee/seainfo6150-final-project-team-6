7f8477a9d4687aa7866518ba9cae6e755ef04824
Shouting Lyu <lyu.s@husky.neu.edu>
2019-12-03 00:31:50 -0800

    Add css to restaurants category page.

--- src/Restaurant/RestaurantGeneral.jsx
+++ src/Restaurant/RestaurantGeneral.jsx
@@ -1,24 +1,30 @@
 import React, {Component} from "react";
-import  { Link } from 'react-router-dom';
 
 export default class RestaurantGeneral extends Component { 
-	
 	constructor(props) {
 	    super(props);
 	    this.state = {
 	    	category: props.category,
 	    	index: props.index,
 	    	url: props.restaurant.image_url,
-	    	name: props.restaurant.name
+	    	restaurantUrl: props.restaurant.url,
+	    	name: props.restaurant.name,
+	    	rating: props.restaurant.rating,
+	    	price: props.restaurant.price
 	    };
   	}	
 
 	render() {
 	    return (
-	    	<div>
-	    		<img className = "restaurant-general-img" src={this.state.url} alt={this.state.title} />
-	    		<h>{this.state.name}</h>
-	    		<a href= {"/restaurants/" + this.state.category + "/" + this.state.index}> Details </a>
+	    	<div className="restaurant-general">
+	    		<div className="image">
+	    			<img src={this.state.url} alt={this.state.title} />
+	    		</div>
+	    		<div className="content">
+	    			<a className= "name item" href={this.state.restaurantUrl}>{this.state.name}</a> 
+	    			<span className="description item">Rating: {this.state.rating} Price:{this.state.price}</span>
+	    			<a className="details grey item" href={"/restaurants/" + this.state.category + "/" + this.state.index}> Details </a>
+	    		</div>
 	    	</div>
 	    );
 	}
